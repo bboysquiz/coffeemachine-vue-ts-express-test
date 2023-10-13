c6b407dae78ee6551821688088bbe76c7160d968
bboysquiz <mrkiraller@gmail.com>
2023-10-13 11:08:37 +0300

    edit main.ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,22 @@
 </script>
 
 <template>
-    <router-view></router-view>
+    <div class="main">
+        <header class="header">
+            <nav class="header-nav">
+                <ul class="header-list">
+                    <li class="header__list-item">
+                        <router-link class="saved-configuration-link" :to="{ name: 'configurator' }"></router-link>
+                    </li>
+                    <li class="header__list-item">
+                        <router-link class="saved-configuration-link" :to="{ name: 'savedconfigs' }"></router-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <router-view></router-view>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
